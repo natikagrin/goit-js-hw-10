de92aea981731b86ae9745c44254e127d1d66bf6
natikagrin <natikagrin@gmail.com>
2022-08-30 21:44:17 +0300

    додані файли js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import './css/styles.css';
-
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { fetchCountries } from './fetchCountries';
+import {
+  createCuntriesListMarkup,
+  createCuntryInfoMarkup,
+} from './createMarkup';
 const DEBOUNCE_DELAY = 300;
