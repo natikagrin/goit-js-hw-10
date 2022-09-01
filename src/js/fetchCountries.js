364bc99a039ddeda054adf6374c68fc7a02c034f
natikagrin <natikagrin@gmail.com>
2022-09-01 21:46:46 +0300

    зміни в сreateMarkup

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,4 +1,4 @@
-
+/*
 // Функция, которая фетчит публичный API
 function fetchCounries(searchQuery) {
   return fetch(`https://restcountries.com/v3.1/name/${searchQuery}`)
@@ -8,4 +8,17 @@ function fetchCounries(searchQuery) {
 }
 
 // Экспорт функции во внешний код
-export default fetchCounries;
+export default fetchCounries;*/
+
+const URL = 'https://restcountries.com/v3.1/name/';
+
+export function fetchCountries(name) {
+  const fields = ['name', 'capital', 'population', 'flags', 'languages'];
+
+  return fetch(`${URL}${name}?fields=${fields.join(',')}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+}
